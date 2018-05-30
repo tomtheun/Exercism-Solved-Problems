8eb13d8ab749d20fa65aa2b9598aaaef1cc12d4b
Tomas Theunissen <tomas_theunissen@hotmail.com>
2018-05-30 14:31:21 +0200

    Typescript - simple-cipher

--- typescript/simple-cipher/simple-cipher.ts
+++ typescript/simple-cipher/simple-cipher.ts
@@ -1,63 +1,60 @@
 class SimpleCipher {
 
+  key: string
+
   constructor(key?: string) {
-    if (key) {
-      var regex = /^[a-z]+$/g
+    if (key !== undefined) {
+      const regex = /^[a-z]+$/g
       if (!regex.test(key)) {
         throw 'Bad key'
       }
+
+      this.key = key;
     } else {
       this.key = [...Array(100)].map(_ => {
-          return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-      }).join('')
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+      }).join('');
     }
   }
 
-    foo = {
-      'A': 'D',
-      'B': 'E',
-      'C': 'F',
-      'D': 'G',
-      'E': 'H',
-      'F': 'I',
-      'G': 'J',
-      'H': 'K',
-      'I': 'L',
-      'J': 'M',
-      'K': 'N',
-      'L': 'O',
-      'M': 'P',
-      'N': 'Q',
-      'O': 'R',
-      'P': 'S',
-      'Q': 'T',
-      'R': 'U',
-      'S': 'V',
-      'T': 'W',
-      'U': 'X',
-      'V': 'Y',
-      'W': 'Z',
-      'X': 'A',
-      'Y': 'B',
-      'Z': 'C',
-    }
+  encode(text: string) {
+    const that = this;
+    let i = 0;
 
-    shiftBy = 3
-    key: string
+    return text.split('')
+      .map(letter => {
+        const key = that.key[i++ % that.key.length];
+        const letterCharCode = letter.charCodeAt(0);
+        const keyCharCode = key.charCodeAt(0);
 
-    encode(text: string) {
-        return text.split('')
-            .map(letter => {
-              return String.fromCharCode(letter.charCodeAt(0) + this.shiftBy);
-            })
-            .join('');
+        let shift = keyCharCode + (letterCharCode - 97);
+        if (shift > 122) {
+          shift = shift - 123 + 97;
+        }
 
-        // iamapandabear
-        // ldpdsdqgdehdu
+        return String.fromCharCode(shift);
+      })
+      .join('');
     }
 
     decode(text: string) {
-        if (text) {}
+      const that = this;
+      let i = 0;
+
+      return text.split('')
+        .map(letter => {
+          const key = that.key[i++ % that.key.length];
+          const letterCharCode = letter.charCodeAt(0);
+          const keyCharCode = key.charCodeAt(0);
+
+          let shift = 97 - (keyCharCode - letterCharCode);
+          if (shift < 97) {
+            shift = 123 - (97 -shift);
+          }
+
+          return String.fromCharCode(shift);
+        })
+        .join('');
     }
 }
 
